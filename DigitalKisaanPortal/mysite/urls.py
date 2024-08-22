3c8e5780478074c34cdef96089d90240bdf4c7df
Purva-Wankhade <purva.wankhade21@vit.edu>
2024-08-22 14:09:19 +0530

    first commit

---
+++ DigitalKisaanPortal/mysite/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index,name='home'),
+    path('about', views.about,name='about'),
+    path('prediction', views.prediction,name='prediction'),
+    path('schemes', views.schemes,name='schemes'),
+    #path('latestnews', views.latestnews,name='latestnews'),
+
+    path('latestnews', views.latestnews,name='latestnews'),
+
+    path('organic-farming/', views.organicfarming, name='organicfarming'),
+
+    path('livefeedpage', views.livefeedpage,name='livefeedpage'),
+    path('community', views.community,name='community'),
+    path('contact', views.contact,name='contact'),
+    path('calculate/', views.index1, name="calculate"),
+]
+
